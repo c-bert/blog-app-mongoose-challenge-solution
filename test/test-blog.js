d205471b2237ba5b211a4ffc917601db1db46b8f
c-bert <danceher76@gmail.com>
2019-01-06 21:08:08 -0500

    creating test-blog.js file

--- test/test-blog.js
+++ test/test-blog.js
@@ -7,3 +7,20 @@ const mongoose = require('mongoose');
 
 const expect = chai.expect;
 
+const {BlogPost} = require('../models');
+const { runServer, app, closeServer } = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
+
+//use randomish data to seed a database using Faker library
+function seedData() {
+    console.info('seeding blogging data');
+    const seedData = [];
+
+    for (let i=1; i<=0; i++) {
+        seedData.push(generateBlogData());   
+    }
+    //this will return a promise
+    return BlogPost.insertMany(seedData);
+}
